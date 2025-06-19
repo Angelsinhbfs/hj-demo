6165ea3c12d7805d3f1d4f0e789650021c23a707
sidney fernandez <angelsinhbfs@gmail.com>
2025-06-18 21:39:17 -0700

    disable debug controls on device

--- Assets/HumanJoystick/Scripts/DebugControls.cs
+++ Assets/HumanJoystick/Scripts/DebugControls.cs
@@ -13,12 +13,19 @@ namespace HumanJoystick.Scripts
         public InputActionReference look;
         public InputActionReference move;
 
+#if !UNITY_EDITOR
+        private void Start()
+        {
+            gameObject.SetActive(false);
+        }
+#endif
         private void Update()
         {
+            //look around, but only horizontally
             camTrans.Rotate(0, look.action.ReadValue<Vector2>().x * lookSpeed, 0);
             controlOrigin.forward = camTrans.forward;
+            //easy move relative to look direction
             inputVector = move.action.ReadValue<Vector2>();
-            //inputVector.y = Input.GetAxis("Vertical");
             camTrans.position = controlOrigin.TransformPoint(inputVector.x, 1.5f, inputVector.y);
         }
     }
