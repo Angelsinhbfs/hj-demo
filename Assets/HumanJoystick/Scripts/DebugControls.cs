02860ebedef2d8dd569fdb320d73bec4f7ace127
sidney fernandez <angelsinhbfs@gmail.com>
2025-06-18 21:37:17 -0700

    add some editor controls

---
+++ Assets/HumanJoystick/Scripts/DebugControls.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace HumanJoystick.Scripts
+{
+    public class DebugControls : MonoBehaviour
+    {
+        public float lookSpeed;
+        public Transform controlOrigin;
+        public Transform camTrans;
+        public Vector2 inputVector = new Vector2();
+        public InputActionReference look;
+        public InputActionReference move;
+
+        private void Update()
+        {
+            camTrans.Rotate(0, look.action.ReadValue<Vector2>().x * lookSpeed, 0);
+            controlOrigin.forward = camTrans.forward;
+            inputVector = move.action.ReadValue<Vector2>();
+            //inputVector.y = Input.GetAxis("Vertical");
+            camTrans.position = controlOrigin.TransformPoint(inputVector.x, 1.5f, inputVector.y);
+        }
+    }
+}
